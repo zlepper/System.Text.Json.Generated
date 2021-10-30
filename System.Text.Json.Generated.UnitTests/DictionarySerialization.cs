5dc1901754b82f6093f801572fe98e63b4935e1a
Zlepper <hansen13579@gmail.com>
2021-10-30 16:54:52 +0200

    Moar tests for mixed list/dictionaries

--- System.Text.Json.Generated.UnitTests/DictionarySerialization.cs
+++ System.Text.Json.Generated.UnitTests/DictionarySerialization.cs
@@ -9,6 +9,7 @@ namespace System.Text.Json.Generated.UnitTests
     public class DictionarySerialization : BaseTests
     {
         private const string DictionaryTypeName = "global::System.Collections.Generic.Dictionary";
+        private const string ListTypeName = "global::System.Collections.Generic.List";
         
         private string WriteDictionary(string propertyName)
         {
@@ -109,6 +110,57 @@ namespace System.Text.Json.Generated.UnitTests
             var expectedNested = SimpleWriteCall("Int1", "WriteNumber", "NestedClass");
 
             VerifyMainGenerator.RunSimpleTest(new []{code, nestedClass}, new []{expected, expectedNested}, new []{"MyCode.MyClass", "MyCode.NestedClass"}, new WellKnownDictionary("string", DictionaryTypeName, new WellKnownDictionary("int", DictionaryTypeName, new SerializableValueType("global::MyCode.NestedClass"))));
+        }
+        
+        
+        [Test]
+        public void HandleDictionaryWithList()
+        {
+            var code = GetCode("Dictionary<string, List<int>>", "MyDict", "new()");
+
+            var expected = WriteDictionary("MyDict");
+
+            VerifyMainGenerator.RunSimpleTest(code, expected, "MyCode.MyClass", new WellKnownDictionary("string", DictionaryTypeName, new WellKnownList(ListTypeName, new WellKnownValueType("int"))));
+        }
+        
+        [Test]
+        public void HandleDictionaryWithListWithDictionary()
+        {
+            var code = GetCode("Dictionary<string, List<Dictionary<int, string>>>", "MyDict", "new()");
+
+            var expected = WriteDictionary("MyDict");
+
+            VerifyMainGenerator.RunSimpleTest(code, expected, "MyCode.MyClass", new WellKnownDictionary("string", DictionaryTypeName, new WellKnownList(ListTypeName, new WellKnownDictionary("int", DictionaryTypeName, new WellKnownValueType("string")))));
+        }
+        
+        [Test]
+        public void HandleListWithListWithDictionary()
+        {
+            var code = GetCode("List<List<Dictionary<int, string>>>", "MyList", "new()");
+
+            var expected = WriteDictionary("MyList");
+
+            VerifyMainGenerator.RunSimpleTest(code, expected, "MyCode.MyClass", new WellKnownList(ListTypeName, new WellKnownList(ListTypeName, new WellKnownDictionary("int", DictionaryTypeName, new WellKnownValueType("string")))));
+        }
+        
+        [Test]
+        public void HandleListWithDictionaryWithDictionary()
+        {
+            var code = GetCode("List<Dictionary<string, Dictionary<int, string>>>", "MyList", "new()");
+
+            var expected = WriteDictionary("MyList");
+
+            VerifyMainGenerator.RunSimpleTest(code, expected, "MyCode.MyClass", new WellKnownList(ListTypeName, new WellKnownDictionary("string", DictionaryTypeName, new WellKnownDictionary("int", DictionaryTypeName, new WellKnownValueType("string")))));
+        }
+        
+        [Test]
+        public void HandleListWithDictionaryWithList()
+        {
+            var code = GetCode("List<Dictionary<string, List<string>>>", "MyList", "new()");
+
+            var expected = WriteDictionary("MyList");
+
+            VerifyMainGenerator.RunSimpleTest(code, expected, "MyCode.MyClass", new WellKnownList(ListTypeName, new WellKnownDictionary("string", DictionaryTypeName, new WellKnownList(ListTypeName, new WellKnownValueType("string")))));
         }
     }
 }
